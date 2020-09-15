d7c305d30303a87c0c649ded45bfd6b7fae59589
Ilyanast <miningilya4@gmail.com>
2020-09-16 00:33:54 +0300

    added unit test project

---
+++ Lab_1.Test/Lab_1.Test.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab1Test
+{
+	TEST_CLASS(Lab1Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
